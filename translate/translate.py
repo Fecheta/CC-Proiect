eb121d50b2595b414b45f43eedf9cf658eefac20
Cristian Fartade <fartade.cristian@gmail.com>
2022-05-23 21:27:49 +0300

    translate si tts

--- translate/translate.py
+++ translate/translate.py
@@ -5,19 +5,20 @@ from key_vault_secrets import get_secret
 class Translate:
 
     def __init__(self):
-        self.key = 'bd1c57e303a145e683c3b139ba2928d3'
+        self.key = 'd8261ea3fa0e46fcbfcaa2f40579978f'
         # self.key = self.get_key()
         self.location = "northeurope"
-        self.endpoint = "https://api.cognitive.microsofttranslator.com/translate"
+        self.endpointTranslate = "https://api.cognitive.microsofttranslator.com/translate"
+        self.endpointDetect = "https://api.cognitive.microsofttranslator.com/detect"
 
     def get_key(self):
         key = get_secret('translate-key')
         return key
 
-    def translate(self, text):
+    def translate(self, text, toLanguage):
         params = {
             'api-version': '3.0',
-            'to': ['de']
+            'to': [toLanguage]
         }
 
         headers = {
@@ -32,7 +33,34 @@ class Translate:
             'text': f"{text}"
         }]
 
-        request = requests.post(self.endpoint, params=params, headers=headers, json=body)
+        request = requests.post(self.endpointTranslate, params=params, headers=headers, json=body)
         response = request.json()
+        print(response)
 
         return response
+
+    def detect(self, text):
+        params = {
+            'api-version': '3.0'
+        }
+
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.key,
+            'Ocp-Apim-Subscription-Region': self.location,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
+
+        # You can pass more than one object in body.
+        body = [{
+            'text': f"{text}"
+        }]
+
+        request = requests.post(self.endpointDetect, params=params, headers=headers, json=body)
+        response = request.json()
+        print(response)
+        if response[0]['score'] > 0.9:
+            language = response[0]['language']
+        else:
+            language = 'en'
+        return language
--- translate/translate_app.py
+++ translate/translate_app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from translate.translate import Translate
+
+
 
 app = Flask(__name__)
 
 
-@app.route('/translate/<value>')
-def translate(value):
-    trs = Translate()
-    response = trs.translate(value)
-    print(response[0])
-    return render_template('translate.html', result=response[0])
+@app.route('/translate', methods=['GET', 'POST'])
+def translate():
+    if request.method == "GET":
+        return render_template('translate.html')
+    if request.method == "POST":
+        text = request.form["text"]
+        to_language = request.form["select"]
+        translation = Translate()
+        result = translation.translate(text, toLanguage=to_language)
+        return render_template('translate.html', result=result)
+    return render_template('translate.html')
 
 
 if __name__ == '__main__':
